41d60268f543b988f55cde8b0eb2042271d58440
cindyhuang1013 <cindy2421342235@gmail.com>
2025-03-31 21:26:02 -0400

    VARDCCGARCH done

--- VAR-DCC-GARCH.R
+++ VAR-DCC-GARCH.R
@@ -13,35 +13,45 @@ library(lattice)
 library(ggplot2)
 
 
-#### Read data and data processing ####
-price <- read.xlsx("data.xlsx")
+#### Read data ####
+workpath = getwd()
+Binance = read.csv(file.path(workpath, "data", "BinanceCoin.csv"))
+Bitcoin = read.csv(file.path(workpath, "data", "bitcoin.csv"))
+Ethereum = read.csv(file.path(workpath, "data", "Ethereum.csv"))
+View(Binance)
 
+#### Extract 'Close' and 'Date' Column  (three dataset has the same Date range)
+Binance = Binance[, c("Date", "Close")]
+Bitcoin = Bitcoin$Close
+Ethereum = Ethereum$Close
+df = data.frame(Date = Binance$Date, 
+                Binance_Close = Binance$Close, 
+                Bitcoin_Close = Bitcoin, 
+                Ethereum_Close = Ethereum)
 
-#transform to numeric and date format
-price$time = strptime(price$time,format = "%Y/%m/%d")
-i=seq(2,10)
-price[ , i] <- apply(price[, i], 2,           
-                     function(x) as.numeric(as.character(x)))
+#transform to Date format
+df$Date = as.Date(df$Date)
 
 #transform to time series data (daily data frequency = 365)
-for(i in 2:ncol(price)){
-  price[,i]=ts(price[,i],start=c(2011,1,1),frequency = 365)
-}
+#for(i in 2:ncol(price)){
+#  price[,i]=ts(price[,i],start=c(2011,1,1),frequency = 365)
+#}
 
 #### Missing Values Imputation ####
-imp_price=data.frame(price$time)
-for (i in 2:length(colnames(price))){
-  new=data.frame(na_interpolation(price[,i]))
-  imp_price=cbind(imp_price,new)
-}
+## Checking if there are missing values
+statsNA(df$Binance_Close)
+statsNA(df$Bitcoin_Close)
+statsNA(df$Ethereum_Close)
 
-#### Get return series ####
-diff_imp_price=data.frame(diff(log(imp_price[,2]))*100)
-for (i in 3:length(colnames(imp_price))){
-  a=diff(log(imp_price[,i]))*100
-  new=data.frame(a)
-  diff_imp_price=cbind(diff_imp_price,new)
+
+#### Get log return series ####
+log_diff_df = data.frame(Date = df$Date[-1])
+for (i in 2:ncol(df)){
+  col_name <- colnames(df)[i]
+  diff_col = diff(log(df[,i]))*100
+  log_diff_df[[col_name]] <- diff_col
 }
+View(log_diff_df)
 
 #### descriptive data ####
 des = function(x){
@@ -55,43 +65,53 @@ des = function(x){
   list = data.frame(mu,max,min,std,kur,ske,JB)
   return(list)
 }
-desdata = des(diff_imp_price[,1])
-for (i in 2:9){
-  t = des(diff_imp_price[,i])
-  desdata = rbind(desdata,t)
+
+desdata = data.frame()
+for (i in 2:ncol(log_diff_df)){
+  col_des = des(log_diff_df[,i])
+  rownames(col_des) = colnames(log_diff_df)[i]
+  desdata = rbind(desdata, col_des)
 }
+desdata
 
 #### correlation matrix ####
-cormatrix = cor(diff_imp_price)
+cormatrix = cor(log_diff_df[,c(2:4)])
+cormatrix
 
 #### check stationary ####
-adf.test(diff_imp_price[,1])
+adf.test(log_diff_df[,2]) # stationary
+adf.test(log_diff_df[,3]) # stationary
+adf.test(log_diff_df[,4]) # stationary
 
 #### check Arch effect ####
-at = scale(diff_imp_price,center = T,scale = F) #remove sample means
+at = scale(log_diff_df[,c(2:4)],center = T,scale = F)  #remove sample means
 MarchTest(at)
 
-#### Build Var model (choose order) ####
-VARselect(diff_imp_price , lag.max = 20) 
-fit=vars::VAR(diff_imp_price,p=7)
+#### Implement VAR model (choose order) ####
+VARselect(log_diff_df[,c(2:4)] , lag.max = 25) 
+fit=vars::VAR(log_diff_df[,c(2:4)],p=1)
 stargazer(fit[["varresult"]],type="text",out = "fit_VAR.html")
-fit$varresult$EN1
 summary(fit)
 
 #### VAR-DCC-GARCH model ####
 spec1 <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
                     mean.model = list(armaOrder = c(1, 1)),distribution.model = "norm")
-mspec1 <- multispec(replicate(9,spec1))
-modelspec1 = dccspec(uspec = mspec1,VAR = TRUE, lag = 7, dccOrder = c(1,1), distribution = "mvt") #t-distribution
-modelfit_all <- dccfit(modelspec1, data = diff_imp_price)
+mspec1 <- multispec(replicate(3,spec1))
+modelspec1 = dccspec(uspec = mspec1,VAR = TRUE, lag = 1, dccOrder = c(1,1), distribution = "mvt") #t-distribution
+modelfit_all <- dccfit(modelspec1, data = log_diff_df[,c(2:4)])
 modelfit_all
 
 modelfit_all@model[["varcoef"]] #get VAR coefficient
 
 #### plot the result ####
-## covariance ##
-plot(rcov(modelfit_all)[2,3,])
+## covariance plot ##
+dim(rcov(modelfit_all))
+plot(rcov(modelfit_all)[1,2,], type = 'l', main = 'Binance Coin and Bitcoin covariance', 
+     xlab = 'Time', ylab = 'Covariance',xaxt = 'n')
+axis(side = 1, at = c(1,277,seq(642,1738,365)),labels = seq(2020,2025,1))
 
 ## correlation ##
-plot(rcor(modelfit_all)[2,3,])
+plot(rcor(modelfit_all)[1,2,], type = 'l', main = 'Binance Coin and Bitcoin correlation', 
+     xlab = 'Time', ylab = 'correlation',xaxt = 'n')
+axis(side = 1, at = c(1,277,seq(642,1738,365)),labels = seq(2020,2025,1))
 
